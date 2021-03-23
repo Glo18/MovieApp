fe0b86445a788625e1c4c8a6d150ebd4617ed352
Glo18 <kiogoraspecial@gmail.com>
2021-03-23 21:33:36 +0300

    use arrayAdapters

--- app/src/main/java/com/moringaschool/movieapp/MoviesArrayAdapter.java
+++ app/src/main/java/com/moringaschool/movieapp/MoviesArrayAdapter.java
@@ -1,4 +1,29 @@
 package com.moringaschool.movieapp;
 
-public class MoviesArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MoviesArrayAdapter extends ArrayAdapter {
+    Context mContext ;
+    String [] mMovies;
+    String [] mGenres;
+
+    public MoviesArrayAdapter(Context mContext, int resource, String[] movies, String[] genres) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mMovies = movies;
+        this.mGenres = genres;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String movie = mMovies[position];
+        String genre = mGenres[position];
+        return String.format("%s \n what a favorite: %s", movie, genre);
+    }
+
+    @Override
+    public int getCount() {
+        return mMovies.length;
+    }
 }
