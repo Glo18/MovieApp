69e8305f1d44217ed04906023f6851946a8222f9
Glo18 <kiogoraspecial@gmail.com>
2021-03-23 21:28:32 +0300

    create new java class

--- app/src/main/java/com/moringaschool/movieapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/movieapp/MainActivity.java
@@ -5,22 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static butterknife.ButterKnife.bind;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.FindMoviesButton) Button mFindMoviesButton;
+    @BindView(R.id.GenresEdit) EditText mGenresEdit;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFindMoviesButton.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v) {
+        ButterKnife.bind(this);
 
-        Intent intent = new Intent(MainActivity.this, MoviesActivity.class);
-        startActivity(intent);
-        Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_LONG).show();
+        mFindMoviesButton.setOnClickListener((View.OnClickListener) this);
     }
 }
