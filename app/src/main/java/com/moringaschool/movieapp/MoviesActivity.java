82fc97546057655f9c170401d6c353b6faf66424
Glo18 <kiogoraspecial@gmail.com>
2021-03-23 14:07:44 +0300

    use of adapters,listview and toasts

--- app/src/main/java/com/moringaschool/movieapp/MoviesActivity.java
+++ app/src/main/java/com/moringaschool/movieapp/MoviesActivity.java
@@ -4,13 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MoviesActivity extends AppCompatActivity {
+
+    private String[] movies = new String[]{
+            "Insidious", "Equalizer",
+            "War Room", "Bay Watch", "Lucy", "Love and Lust",
+            "The mechanic resurrection", "Transporter"
+    };
+
+    private String[] genres = new String[]{
+            "Fantasy", "War", "Romance",
+            "Action", "Horror", "Drama", "Comedy", "Thriller"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+        MoviesArrayAdapter adapter = new MoviesArrayAdapter(this, android.R.layout.simple_list_item_1, movies, genres);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String movies = ((TextView) view).getText().toString();
+                Toast.makeText(MoviesActivity.this, movies, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String genres = intent.getStringExtra("genres");
         mGenreTextView.setText("Available genres: " + genres);
