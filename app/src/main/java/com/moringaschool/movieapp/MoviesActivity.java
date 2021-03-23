9156ebc296d055457ac881a1d0000e1f3aae0558
Glo18 <kiogoraspecial@gmail.com>
2021-03-23 13:31:23 +0300

    pass data with intent

--- app/src/main/java/com/moringaschool/movieapp/MoviesActivity.java
+++ app/src/main/java/com/moringaschool/movieapp/MoviesActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MoviesActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MoviesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+        Intent intent = getIntent();
+        String genres = intent.getStringExtra("genres");
     }
 }
