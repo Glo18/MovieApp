bd003e7aa93d4e53bde9bee3c8485ab061b1d0b8
Glo18 <kiogoraspecial@gmail.com>
2021-04-06 20:02:13 +0300

    add new button

--- app/src/main/java/com/moringaschool/movieapp/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/moringaschool/movieapp/adapters/FirebaseRestaurantViewHolder.java
@@ -1,4 +1,59 @@
 package com.moringaschool.movieapp.adapters;
 
-public class FirebaseRestaurantViewHolder {
+import android.content.Intent;
+import android.os.Parcel;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.Context;
+import com.moringaschool.movieapp.Constants;
+import com.moringaschool.movieapp.models.Result;
+import com.moringaschool.movieapp.ui.MovieDetailActivity;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    View mView;
+    Context mContext;
+
+    public FirebaseRestaurantViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList< > movies = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_MOVIES);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : dataSnapshot.getChildren()) {
+                    movies.add(snapshot.getValue(Result.class));
+                }
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, MovieDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("movies", Parcels.wrap(movies));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
 }
--- app/src/main/java/com/moringaschool/movieapp/models/Result.java
+++ app/src/main/java/com/moringaschool/movieapp/models/Result.java
@@ -3,8 +3,11 @@ package com.moringaschool.movieapp.models;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.parceler.Parcel;
+
 import java.util.List;
 
+@Parcel
 public class Result {
     @SerializedName("adult")
     @Expose
